5d870d7a15c22cd36e17cb8a69fb6c1247142238
pohern <ohern_p@yahoo.com>
2021-07-13 19:54:12 -0500

    Add cypress and implement for end-end testing

---
+++ client/cypress/integration/home/app.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+
+describe("Home page with default data", () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3001');
+    });
+    it("Displays three restaurants", () => {
+        cy.get("#restaurants-display").children().should("have.length", 3);
+    });
+});
