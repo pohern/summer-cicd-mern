5f2250c0c14b401d08c431907020ab0b74b0c35f
pohern <ohern_p@yahoo.com>
2021-07-27 08:24:33 -0500

    Add cypres test and have it run seed

--- client/cypress/integration/home/app.spec.js
+++ client/cypress/integration/home/app.spec.js
@@ -4,9 +4,18 @@
 
 describe("Home page with default data", () => {
     beforeEach(() => {
+        cy.exec('cd .. && npm run seed')
         cy.visit('http://localhost:3001');
     });
     it("Displays three restaurants", () => {
         cy.get("#restaurants-display").children().should("have.length", 3);
     });
+    it("Updates the input value", () => {
+        cy.get('#name').type("Waffle House");
+        cy.get('#name').should('have.value', 'Waffle House');
+    });
+    it("Should submit the form", () => {
+        cy.get("#name").type("Waffle House{enter}");
+        cy.get("#restaurants-display").children().should("have.length", 4);
+    })
 });
